342190ebc32d1c84923cec9930e94627efca8e77
jixenc <jixenc@miumg.edu.gt>
2022-10-22 15:18:34 -0600

    first commit

--- src/app/shared/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    userName: String;
-    password: String;
-
-    constructor (userName: string, password: string){
-      this.userName = userName;
-      this.password = password;
-    }
-  }
